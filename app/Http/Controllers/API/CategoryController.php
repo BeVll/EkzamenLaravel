ae77681451db3e3424752ca2661595c71f165727
Владислав Берестюк <vladixerplay@gmail.com>
2023-04-29 18:02:41 +0300

    crud

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -4,17 +4,50 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Faker\Core\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
-    public  function index(){
+    public function index(){
         $categories = Category::all();
         return response()->json($categories);
     }
+    public function show(int $id){
+        $category = Category::where('id', $id)->first();
+        return response()->json($category);
+    }
+    public function update(Request $request, $id){
+        $input = $request->all();
+        $deleteImage = Category::where('id', $id)->first()->image;
 
+        Storage::disk("local")->delete("public/images/categories/".$deleteImage);
+        $filename = uniqid(). '.' .$request->file('image')->getClientOriginalExtension();
+
+        Storage::disk('local')->put("public/images/categories/".$filename,file_get_contents($request->file("image")));
+        $input["image"] = $filename;
+        print($input["image"]);
+        if($input["status"])
+            $input["status"] = 1;
+        else
+            $input["status"] = 0;
+        $category = Category::where('id', $input["id"])->update($input);
+        return response()->json($category);
+    }
     public function store(Request $request){
         $input = $request->all();
+
+            $filename = uniqid(). '.' .$request->file('image')->getClientOriginalExtension();
+
+            Storage::disk('local')->put("public/images/categories/".$filename,file_get_contents($request->file("image")));
+        $input["image"] = $filename;
+            print($input["image"]);
+        if($input["status"])
+            $input["status"] = 1;
+        else
+            $input["status"] = 0;
+
         $category = Category::create($input);
         return response()->json($category);
     }
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/categories', [CategoryController::class, "index"]);
+Route::get('/category/{id}', [CategoryController::class, "show"]);
 Route::post('/categories', [CategoryController::class, "store"]);
+Route::put('/category/{id}', [CategoryController::class, "update"]);
 Route::delete('/categories/{id}', [CategoryController::class, "delete", []]);
